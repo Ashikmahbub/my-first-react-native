f2b8c36ee3f5db51c5b94bd8e98bf2f26820f71f
Ashikmahbub <ashikmahbub56@gmail.com>
2024-01-11 23:57:37 +0600

    app-folder-created

---
+++ app/_layout.js
@@ -0,0 +1,12 @@
+import {Stack} from 'expo-router';
+import React from 'react'
+
+const Layout = () => {
+  return (
+    <div>
+      <Stack/>
+    </div>
+  )
+}
+
+export default Layout
---
+++ app/index.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { View ,Text} from 'react-native';
+
+const Home = () => {
+  return (
+    <div>
+      <View>
+        <Text>Home</Text>
+      </View>
+    </div>
+  )
+}
+
+export default Home
